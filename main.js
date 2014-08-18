d6e0b0b85e3898bb12af558cff987732b5a9ddf7
Phil Welsh <phizs@hotmail.com>
2014-08-18 19:26:38 +0100

    first

---
+++ main.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+    $('#fullpage').fullpage({
+        'slidesColor': ['#AAA', '#FFB', '#0BD'],
+        'scrollingSpeed': 700,
+        'anchors': ['first', 'second', 'third', 'forth', 'fifth'],
+        'controlArrowColor': '#000',
+        'navigation': true,
+        'navigationPosition': 'right',
+        'navigationColor': '#000',
+        'navigationTooltips': [],
+
+        'slidesNavigation': true,
+        'slidesNavPosition': 'bottom',
+        'controlArrowColor': '#000',
+        'loopBottom': true,
+        'loopTop': false,
+        'loopHorizontal': true,
+        'autoScrolling': false,
+        'scrollOverflow': true,
+
+        //afterRender: function () {//autoslides slides
+        //    setInterval(function () {
+        //        $.fn.fullpage.moveSlideRight();
+        //    }, 2000);
+        //}
+    });
+
+    $('.slip').on('click', function (e) {
+        var $this = $(e.currentTarget)
+        if($this.hasClass('move')){
+            $this.parent().children().removeClass('move')
+            setTimeout(function () {
+                $this.parent().children().addClass('move');
+            }, 1000)        
+        } else {
+            $this.parent().children().addClass('move');
+        }
+
+        $('.pers').on('click', function () {
+            setTimeout(function () {
+                $('.abilitytech').addClass('displaynone')
+                $('.abilitypers').removeClass('displaynone');
+            }, 800);
+        });
+        $('.tech').on('click', function () {
+            setTimeout(function () {
+                $('.abilitypers').addClass('displaynone')
+                $('.abilitytech').removeClass('displaynone');
+            }, 800);
+        });
+    })
+
+    //if ($('.four').hasClass('active')) {
+    //    $('.wrap').addClass('anim')
+    //}else{}
+    
+});
